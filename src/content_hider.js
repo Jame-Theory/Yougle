a724d3b5f44f7139c74a17914e4092f3604fbed2
Jame-Theory <jamesmcaporuscio@gmail.com>
2025-07-10 13:44:45 -0400

    Yougle

---
+++ src/content_hider.js
@@ -0,0 +1,28 @@
+// List of YouTube recommendation elements to hide
+const selectorsToHide = [
+  '#related', // Sidebar related videos
+  'ytd-watch-next-secondary-results-renderer', // Suggested videos below main
+  'ytd-compact-autoplay-renderer', // Autoplay up next
+  'ytp-endscreen-content', // Endscreen suggestions
+  'ytd-rich-grid-renderer', // Home page video grid
+];
+
+function hideRecommendations() {
+  selectorsToHide.forEach(selector => {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(el => {
+      el.style.display = 'none';
+    });
+  });
+}
+
+hideRecommendations();
+
+const observer = new MutationObserver(() => {
+  hideRecommendations();
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
